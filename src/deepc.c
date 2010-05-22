a6a3dea08609bb3d7c1ff577ac408cd9b63f334c
Zik Saleeba <zik@zikzak.net>
2010-05-22 20:55:33 +0200

    Ripped off the guts of minic to use in DeepC

--- src/deepc.c
+++ src/deepc.c
@@ -3,6 +3,35 @@
 #include "deepc.h"
 
 
+/* 
+ * NAME:        Initialise
+ * ACTION:      Initialise any structures before we start
+ */
+ 
+void Initialise()
+{
+    ShareStrInit();
+    DataTypeInit();
+    CodeGenInit();
+}
+
+
+/*
+ * NAME:        Cleanup
+ * ACTION:      Free any resources before we leave
+ * PARAMETERS:  ParseState *Parser - a parser structure to clean up if we have one
+ */
+ 
+void Cleanup(ParseState *Parser)
+{
+    if (Parser != NULL)
+        ParserClose(Parser);
+
+    DataTypeCleanup();
+    ShareStrClose();
+}
+
+
 int main(int argc, char **argv)
 {
     printf("deepc v 0.1\n");
